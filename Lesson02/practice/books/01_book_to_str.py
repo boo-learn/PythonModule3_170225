853c95914015384cf38eb4cc1f38fd07134c1a87
boo <booblegum42@gmail.com>
2025-04-24 22:41:06 +0545

    Lesson02

---
+++ Lesson02/practice/books/01_book_to_str.py
@@ -0,0 +1,29 @@
+class Author:
+    def __init__(self, name: str, surname: str):
+        self.name = name
+        self.surname = surname
+
+    def short_name(self) -> str:
+        # TODO-1: метод возвращает строку в формате: М.Булгаков
+        #  пояснение: Первую букву имени, фамилию целиком
+        ...
+
+
+class Book:
+    def __init__(self, name: str, author: Author, year: int, pages: int):
+        self.name = name
+        self.author = author
+        self.year = year
+        self.pages = pages
+
+    def to_str(self) -> str:
+        # TODO-2: метод возвращает строку в формате: "Вьюга" author:М.Булгаков publish:1926 год
+        #  пояснение: Название книги выводим в кавычках(""), у имени автора только первую букву
+        ...
+
+
+author = Author("Михаил", "Булгаков")
+book = Book("Вьюга", author, 1926, 25)
+
+# TODO-3: Измените имя, фамилию автора и название книги, проверьте, что программа корректно работает с новыми значениями
+print(book.to_str())
