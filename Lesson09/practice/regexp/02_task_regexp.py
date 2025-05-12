11b99881a4eef6a46cd559a9ae054713bf458401
boo <booblegum42@gmail.com>
2025-05-13 03:03:34 +0700

    Lesson09 result

--- Lesson09/practice/regexp/02_task_regexp.py
+++ Lesson09/practice/regexp/02_task_regexp.py
@@ -1,5 +1,10 @@
 # Дано произвольное предложение. Слова разделены пробелами. Предложение содержит знаки препинания.
 # Найти все слова "the" в тексте, игнорируя регистр и убедившись,
 # что это именно слово, а не часть другого слова (например, "there").
+import re
 
 text_modified = "The quick brown fox jumps over there, near the lazy dog. THE end."
+template = r"\bthe\b"
+result = re.findall(template, text_modified.lower())
+
+print(result)
--- Lesson09/practice/regexp/03_task_regexp.py
+++ Lesson09/practice/regexp/03_task_regexp.py
@@ -5,5 +5,19 @@
 # 3) все слова начинающиеся на гласную кириллическую букву
 # 4) все слова начинающиеся на согласную кириллическую букву
 # 5) все уникальные(без дубликатов) знаки препинания (знаками препинания считать символы .,!?;:)
+import re
 
-text = ...
+text = "quick brown fox jumps over there, near the lazy dog. THE end."
+
+# 1
+first_word_template = r"^\w+"
+first_word = re.findall(first_word_template, text)
+print(first_word)
+
+# 2
+...
+
+# 5
+signs_template = r"[.,!?;:]"
+signs = re.findall(signs_template, text)
+print(set(signs))
